9bf84b573599ad9e72be0b767e18adfd56f767df
U-Jengerer-PC\Jengerer <Jengerer@Jengerer-PC.(none)>
2014-02-17 14:56:24 -0500

    Added missing version header.

---
+++ include/jui/jui_version.hpp
@@ -0,0 +1,11 @@
+#ifndef JUI_VERSION_HPP
+#define JUI_VERSION_HPP
+
+#include <string/string.hpp>
+
+namespace JUI
+{
+	const JUTIL::ConstantString JUI_VERSION( "0.1" );
+}
+
+#endif // JUI_VERSION_HPP
