01873efe15b51539c84e9f48c362fb657de9bf59
Evgenii Babinets <evgeniibabinets@gmail.com>
2023-10-04 22:45:32 -0400

    Changing to static library and removing redist dependency.

--- include/jui/jui_base.hpp
+++ include/jui/jui_base.hpp
@@ -1,10 +1,14 @@
 #ifndef JUI_BASE_HPP
 #define JUI_BASE_HPP
 
+#if defined(JUI_DLL)
 #if defined(JUI_EXPORT)
 #define JUI_API __declspec(dllexport)
 #else
 #define JUI_API __declspec(dllimport)
+#endif
+#else
+#define JUI_API
 #endif
 
 #endif // JUI_BASE_HPP
